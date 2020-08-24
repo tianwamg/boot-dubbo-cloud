3d1c68813027de424d791710fc7e803e0e7586f1
wangpp <987897856@qq.com>
2020-08-24 15:58:54 +0800

    initial

---
+++ dubbo-rpc-api/src/test/java/com/ms/api/dubborpcapi/DubboRpcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.api.dubborpcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboRpcApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
