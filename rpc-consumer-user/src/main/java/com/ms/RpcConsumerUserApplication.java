0db907d84f4064f455e2c40db0a19fc5af6bb0d9
wangpp <987897856@qq.com>
2020-08-24 16:20:51 +0800

    init

--- rpc-consumer-user/src/main/java/com/ms/RpcConsumerUserApplication.java
+++ rpc-consumer-user/src/main/java/com/ms/RpcConsumerUserApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RpcComsumerUserApplication {
+public class RpcConsumerUserApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RpcComsumerUserApplication.class, args);
+        SpringApplication.run(RpcConsumerUserApplication.class, args);
     }
 
 }
